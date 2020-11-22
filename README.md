5f9f4fe55f4dd7c29e874944ce887cf712379d96
Li Chengze0612 <72480182+yonghuguge12@users.noreply.github.com>
2020-11-22 15:32:59 +0800

    Update and rename README.md to Share分享.py

--- README.md
+++
@@ -1,1 +0,0 @@
-# My-Program-Pocket
---
+++ Share分享.py
@@ -0,0 +1,10 @@
+# My-Program-Pocket
+import webbrowser
+from time import*
+print("I made the source code of a document web page./我做了一个文件网页的源代码。(html)")
+sleep(1)
+print("打不开的加QQ：3226508740/Add Telegram if you can’t open: @Robin0612")
+sleep(1)
+print("三秒后打开/Turn on in three seconds")
+sleep(3)
+webbrowser.open("https://drive.google.com/file/d/1_5_3_Ne2VU3rY7EdDGLWBMn9dlJGNtIT/view?usp=sharing")
