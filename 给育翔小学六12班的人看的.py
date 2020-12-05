bd4e678590d6b363ccd1715048c995862652bdc0
Li Chengze0612 <72480182+yonghuguge12@users.noreply.github.com>
2020-12-05 11:31:19 +0800

    Create 给育翔小学六12班的人看的.py
    在线解决线上投诉问题
    
    涵养！
    涵养！
    “涵养笔带了多的了吗？我今天忘带了。”
    “把涵养书后面的附页借我一下呗！”
    “涵养是家教的一种体现。”

---
+++ 给育翔小学六12班的人看的.py
@@ -0,0 +1,27 @@
+import time
+print("欢迎来到董老师在线投诉中心")
+print("投诉电话：12345")
+print("投诉受理内容：\n 1.作业问题 \n 2.学科问题 \n 3.学生辅导 \n 4.成绩问题 \n 5.其他")
+a = input("请输入问题序号：")
+if a == "1":
+    input("请输入详细内容：")
+    sleep(1)
+    print("投诉成功！")
+if a == "2":
+    input("请输入详细内容：")
+    sleep(1)
+    print("投诉成功！")
+if a == "3":
+    input("请输入详细内容：")
+    sleep(1)
+    print("投诉成功！")
+is a == "4":
+    input("请输入详细内容：")
+    sleep(1)
+    print("投诉成功！")
+if a == "5":
+    input("请输入详细内容：")
+    sleep(1)
+    print("投诉成功！")
+else:
+    print("没有搜寻到投诉受理内容")
