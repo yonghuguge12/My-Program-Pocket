2d831b28b325e6936acf776c97a6c7f914bfe825
一夜度南山 <72480182+yonghuguge12@users.noreply.github.com>
2021-02-04 19:44:59 +0800

    Create 陌生用户检测.py
    一定要选择开机自启动，并锁定该程序的运行！
    可以改变13行，15行，18行的“xxx“，更加个性化

---
+++ 陌生用户检测.py
@@ -0,0 +1,18 @@
+from time import*
+from os import*
+print("最近老有人动我电脑，整的我六神无主，心神不宁")
+sleep(1)
+system("clear")
+print("素质人才")
+sleep(1)
+system("clear")
+print("等会我会问你一个问题，你答对了，我就让你用一会；答错了，小心今后拉上禁用者清单！")
+sleep(1)
+system("clear")
+answer = input("这台电脑的主人是谁的？请回答：")
+if answer == "xxx":
+	print("请继续使用，感谢您的配合^-^")
+	print("©2021 xxxx Company")
+else:
+	print("我倒要看看是哪个妖魔鬼怪懂我电脑，咱走着瞧")
+	print("©2021 xxxx Company")
