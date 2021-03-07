8ab759def7caab6356002e595ff9630c9266474e
一夜度南山 <72480182+yonghuguge12@users.noreply.github.com>
2021-03-07 16:14:51 +0800

    Create 爬虫.py
    一个小爬虫

---
+++ 爬虫.py
@@ -0,0 +1,11 @@
+import requests, bs4, time
+response = requests.get("https://github.com/")
+response.encoding = "UTF-8"
+soup = bs4.BeautifulSoup(response.text,"lxml")
+data1 = soup.find_all(name="div", class_="article")
+for n in data1:
+    data2 = n.find_all(name="a")
+    print("--------------------------------------")
+    print("题目:"+data2[0].text)
+    print("摘要:"+data2[1].text)
+    print("主题:"+data2[2].text)
